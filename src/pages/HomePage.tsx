d2490cae486dce063be178a9e29ae9443971b2c1
dinhlooc <ngoloc3304@gmail.com>
2025-09-19 20:25:14 +0700

    implement feat/course

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,8 +1,11 @@
+import React from "react";
+import CourseList from "../features/course/CourseList";
+
 const HomePage = () => {
   return (
     <div className="max-w-6xl mx-auto py-10">
       <h1 className="text-3xl font-bold">Welcome to E-Learning</h1>
-      <p className="mt-4">Khám phá các khóa học online.</p>
+      <CourseList />
     </div>
   );
 };
